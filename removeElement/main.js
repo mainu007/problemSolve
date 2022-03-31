dbffc8bfa278e99760c0f6487d8d9ac1ba5a509e
mainu007 <mainudev.co@gmail.com>
2022-03-31 22:15:21 +0600

    :sparkles: Remove element

---
+++ removeElement/main.js
@@ -0,0 +1,37 @@
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in -place.The relative order of the elements may be changed.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums.More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array.You must do this by modifying the input array in -place with O(1) extra memory.
+
+const numbers = [0, 1, 2, 2, 3, 0, 4, 2];
+const value = 2;
+// expected: [0,1,4,0,3]
+
+const removeElement = (nums, val) => {
+  const move = () => {
+    const item = nums.pop();
+    if (item !== val) {
+      return item;
+    }
+    return move();
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      if (i !== (nums.length - 1)) {
+        const moveItem = move();
+        if (moveItem) {
+          nums.splice(i, 1, moveItem);
+        }
+      } else {
+        nums.splice(i, 1);
+      }
+    }
+  }
+  return nums;
+};
+
+console.log(removeElement(numbers, value)); 
